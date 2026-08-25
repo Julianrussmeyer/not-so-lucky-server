3fd18e259dcc1747a318462370e1cf55d29709c6
Julian Russmeyer <julian@flower.ai>
2026-08-25 20:39:12 +0200

    Add Vercel Link to CORS

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://example.com'],
+    origin: ['http://localhost:5173', 'https://not-so-lucky-client.vercel.app'],
   })
 );
 
